92d406b6bed094672d09f8ab847167529ee646c9
Ilie1985 <ilie.marian32@yahoo.com>
2023-01-05 11:22:33 +0000

    calculate most used language

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -6,6 +6,39 @@ import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from "./Charts";
 const Repos = () => {
   const { githubRepos } = useGlobalContext();
 
+  //reduce method ,pass caalback func and the 2nd thing is what we`re trying to return from the reduce,in this case I want to return an object
+
+  //with reduce we have 2 parameters-(1 total-what we`re returning(the object in this case) , 2 is the exact itteration, and we can name them however we want)
+
+  //when working with reduce we must make sure that we`re returning the total
+
+  let languages = githubRepos.reduce((total, item) => {
+    const { language } = item;
+
+    if (!language) {
+      return total;
+    }
+
+    if (!total[language]) {
+      total[language] = { label: language, value: 1 };
+    } else {
+      total[language] = {
+        ...total[language],
+        value: total[language].value + 1,
+      };
+    }
+
+    return total;
+  }, {});
+
+  //transfrom the object into an array of objects
+  languages = Object.values(languages)
+    .sort((a, b) => {
+      return b.value - a.value;
+    })
+    .slice(0, 10);
+
+  console.log(languages);
   const chartData = [
     {
       label: "HTML",
@@ -24,7 +57,7 @@ const Repos = () => {
   return (
     <section className="section">
       <Wrapper className="section-center">
-        <Pie3D data={chartData} />
+        <Pie3D data={languages} />
         {/* <ExampleChart data={chartData} />; */}
       </Wrapper>
     </section>
